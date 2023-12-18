ecf0260e170be0805a9700dc4b1497ed6c2c89ed
igbanibo-Dengi <igbanibodengi@gmail.com>
2023-12-18 02:54:43 +0100

    customied login and sign up pages

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -1,17 +1,42 @@
 import React from 'react'
 import { Metadata } from 'next'
 
-import { Gutter } from '../../_components/Gutter'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
 
 import classes from './index.module.scss'
+import Image from 'next/image'
+import { RenderParams } from '../../_components/RenderParams'
+import Link from 'next/link'
 
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
+    <section className={classes.recoverPassword}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image
+            src="/logo-black.svg"
+            width={250}
+            height={23}
+            className={classes.logo}
+            alt="logo"
+          />
+        </Link>
+      </div>
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+          <Link href="/login">
+            <Image src="/assets/icons/arrow-left.svg" alt="left-arrow" width={24} height={24} />
+            <p>back</p>
+          </Link>
+          <div className={classes.formTitle}>
+            <h3>Forgot password</h3>
+          </div>
+          <RecoverPasswordForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
